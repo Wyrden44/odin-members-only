e90f52926aec1f2c936ce99d9734eb4c3471f5de
Pawel Gurlo <pawel.gurlo@gmail.com>
2025-10-14 11:12:00 +0200

    Set up project packages

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const path = require("node:path");
 const express = require("express");
 const app = express();
 const mainRouter = require("./routers/mainRouter");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
 
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
 
 app.use(mainRouter);
 
@@ -15,5 +20,5 @@ app.listen(process.env.PORT || 3000, err => {
     if (err) {
         throw err;
     }
-    console.log("App is running!");
+    console.log("App is running on port ", process.env.PORT || 3000);
 });
