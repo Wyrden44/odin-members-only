26d96be398849fb6b22a465cc25d87f9c3d6a722
Pawel Gurlo <pawel.gurlo@gmail.com>
2025-10-16 13:15:47 +0200

    Style basic page layout

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const passport = require("passport");
 
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
